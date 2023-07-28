06664a33882da2e68d3ba02795679c7f425946e5
Arthur Kozhemyakin <trable228@icloud.com>
2023-07-28 21:42:55 +0300

    instance retrofit

--- app/src/main/java/com/landfathich/retrofitapp/MainActivity.kt
+++ app/src/main/java/com/landfathich/retrofitapp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.landfathich.retrofitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.landfathich.retrofitapp.retrofit.ProductApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.w3c.dom.Text
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.tv)
+        val button = findViewById<Button>(R.id.btn)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://dummyjson.com/")
+            .addConverterFactory(GsonConverterFactory.create()) // подключаем конвертер, который будет превращать gson формат в data класс
+            .build()
+        val productApi = retrofit.create(ProductApi::class.java)
+
+        button.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val product = productApi.getProductById(3)
+                runOnUiThread {
+                    textView.text = product.title
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/landfathich/retrofitapp/retrofit/ProductApi.kt
+++ app/src/main/java/com/landfathich/retrofitapp/retrofit/ProductApi.kt
@@ -1,4 +1,9 @@
 package com.landfathich.retrofitapp.retrofit
 
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface ProductApi {
+    @GET("product/{id}")
+    suspend fun getProductById(@Path("id") id: Int): Product
 }
