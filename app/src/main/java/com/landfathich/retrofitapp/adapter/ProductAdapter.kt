4c27fc4369372360c1cde302d1dff5efe6e248a7
Arthur Kozhemyakin <trable228@icloud.com>
2023-07-29 02:05:02 +0300

    product adapter

---
+++ app/src/main/java/com/landfathich/retrofitapp/adapter/ProductAdapter.kt
@@ -0,0 +1,44 @@
+package com.landfathich.retrofitapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.landfathich.retrofitapp.R
+import com.landfathich.retrofitapp.databinding.ListItemBinding
+import com.landfathich.retrofitapp.retrofit.Product
+import com.squareup.picasso.Picasso
+
+class ProductAdapter : ListAdapter<Product, ProductAdapter.Holder>(Comparator()) {
+    class Holder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = ListItemBinding.bind(view)
+        fun bind(product: Product) = with(binding) {
+            title.text = product.title
+            desc.text = product.description
+            Picasso.get()
+                .load(product.images[0])
+                .into(preview)
+        }
+    }
+
+    class Comparator : DiffUtil.ItemCallback<Product>() {
+        override fun areItemsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
