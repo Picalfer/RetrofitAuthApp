cecbf938060426f5be364d2751f6951cd3932b25
Arthur Kozhemyakin <trable228@icloud.com>
2023-07-29 11:24:08 +0300

    searchView layout, impl getProductsByName fun

--- app/src/main/java/com/landfathich/retrofitapp/MainActivity.kt
+++ app/src/main/java/com/landfathich/retrofitapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.landfathich.retrofitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView.OnQueryTextListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.landfathich.retrofitapp.adapter.ProductAdapter
 import com.landfathich.retrofitapp.databinding.ActivityMainBinding
@@ -41,13 +42,22 @@ class MainActivity : AppCompatActivity() {
             .build()
         val mainApi = retrofit.create(MainApi::class.java)
 
-        CoroutineScope(Dispatchers.IO).launch {
-            val products = mainApi.getAllProducts()
-            runOnUiThread {
-                binding.apply {
-                    adapter.submitList(products.products)
-                }
+        binding.sv.setOnQueryTextListener(object : OnQueryTextListener {
+            override fun onQueryTextSubmit(text: String?): Boolean { // срабатывает когда человек жмет на кнопку поиска на клавиатуре
+                return true
             }
-        }
+
+            override fun onQueryTextChange(text: String?): Boolean { // срабатывает каждый раз когда есть изменения в тексте searchView
+                CoroutineScope(Dispatchers.IO).launch {
+                    val products = text?.let { mainApi.getProductsByName(it) }
+                    runOnUiThread {
+                        binding.apply {
+                            adapter.submitList(products?.products)
+                        }
+                    }
+                }
+                return true
+            }
+        })
     }
 }
