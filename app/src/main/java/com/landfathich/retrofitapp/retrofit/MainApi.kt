b1a30417430526fedf703fb455129d247137e716
Arthur Kozhemyakin <trable228@icloud.com>
2023-07-29 11:50:48 +0300

    fun auth search products

--- app/src/main/java/com/landfathich/retrofitapp/retrofit/MainApi.kt
+++ app/src/main/java/com/landfathich/retrofitapp/retrofit/MainApi.kt
@@ -2,20 +2,26 @@ package com.landfathich.retrofitapp.retrofit
 
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MainApi {
-    @GET("product/{id}")
+    @GET("auth/product/{id}")
     suspend fun getProductById(@Path("id") id: Int): Product
 
     @POST("auth/login")
     suspend fun auth(@Body authRequest: AuthRequest): User
 
-    @GET("products")
+    @GET("auth/products")
     suspend fun getAllProducts(): Products
 
-    @GET("products/search")
-    suspend fun getProductsByName(@Query("q") name: String): Products
+    @Headers("Content-type: application-gson")
+    @GET("auth/products/search")
+    suspend fun getProductsByNameAuth(
+        @Header("Authorization") token: String,
+        @Query("q") name: String,
+    ): Products
 }
